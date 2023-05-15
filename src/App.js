5d8009084802582bddc849d48aca618657042475
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-05-15 03:58:17 +0200

    Final version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import AllTasks from "./components/AllTasks"
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="all-tasks">
+      <AllTasks />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
+
+
+
+
+//cisty javasctipt - zacatek
+  // const vychoziPole = [5, 6, 7, 8, 9]
+  // const vyslednePole = vychoziPole.map((jednoCislo) => {
+  //   return jednoCislo + 10
+  // })
+  // console.log(vyslednePole)
+  //cisty javasctipt - konec
+
+  //funkce map() - zacatek
+  // const data = [
+  //   { id: 1, name: 'Vynést koš' },
+  //   { id: 2, name: 'Uklidit kuchyň' },
+  //   { id: 3, name: 'Zajít do fitka' },
+  //   { id: 4, name: 'Nakoupit' },
+  //   { id: 5, name: 'Nakrmit kočku' },
+  // ]
+
+  // const vysledek = data.map((jedenUkol) => {
+  //   return jedenUkol.name
+  // })
+  // console.log(vysledek)
+  //funkce map() - konec
+
+  //funkce filter() - zacatek
+  // const data = [
+  //   { id: 1, name: 'Vynést koš' },
+  //   { id: 2, name: 'Uklidit kuchyň' },
+  //   { id: 3, name: 'Zajít do fitka' },
+  //   { id: 4, name: 'Nakoupit' },
+  //   { id: 5, name: 'Nakrmit kočku' },
+  // ]
+
+  // const vymazaneId = 3
+
+  // const vyslednePole = data.filter((jedenUkol) => {
+  //   return jedenUkol.id !== vymazaneId
+  // })
+  // console.log(vyslednePole)
+
+  //funkce filter() - konec
+
